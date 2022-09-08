d13fdc1092a4ffbb750c3339b5317b7ec8b4a62d
meliphyra-ux <xennerit@icloud.com>
2022-09-09 00:09:40 +0300

    Realization of printing numbers

--- js/index.js
+++ js/index.js
@@ -1,12 +1,36 @@
 const buttonsArray = [0,1,2,3,4,5,6,7,8,9,'-','+','=','A/C','%'];
 const buttonContainter = document.querySelector('div#button--container');
+let displayValue = '';
+const inputValue = document.querySelector('input');
 
-buttonsArray.forEach(button=>{
+buttonsArray.forEach(button => createButton(button))
+
+function createButton(button){
     const createdButton = document.createElement('button');
+    createdButton.setAttribute("value", `${button}`)
     createdButton.textContent = button;
     buttonContainter.appendChild(createdButton)
-
+    // if(typeof button == string){
+        
+    // }
+    // else{
+        createdButton.addEventListener("click", (e) => addDigit(e.target.value))
+    // } 
+}
+window.addEventListener("keydown", (e)=>{
+    const pressedKey = document.querySelector(`button[value="${e.key}"]`)
+    pressedKey.click();
 })
+
+function updateDisplay(){
+    inputValue.value = displayValue; 
+}
+
+function addDigit(digit){
+    displayValue += digit;
+    updateDisplay()
+    console.log(displayValue)
+}
 
 function add(firstNumber, secondNumber){
     return firstNumber + secondNumber;
